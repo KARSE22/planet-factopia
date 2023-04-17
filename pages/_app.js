065f375f04ad01ebe3362909ee2a480589bd9183
Karrissa Volcy <v.karrissa1@gmail.com>
2023-04-17 18:19:24 -0400

    loaded variable fonts with next/font

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import "@/styles/globals.css";
 import Layout from "../components/Layout";
+import { Antonio, League_Spartan } from "next/font/google";
+
+const antonio = Antonio({
+  subsets: ["latin"],
+  variable: "--antonio-font",
+});
+const spartan = League_Spartan({
+  subsets: ["latin"],
+  variable: "--spartan-font",
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div className={`${antonio.variable} ${spartan.variable}`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
