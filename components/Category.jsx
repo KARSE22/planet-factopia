fb583db5560c5c91f2beba6b7ff1a95b6fe27fc4
Karrissa Volcy <v.karrissa1@gmail.com>
2023-04-20 19:12:34 -0400

    updated sourceURL on button click

--- components/Category.jsx
+++ components/Category.jsx
@@ -1,15 +1,17 @@
 import styles from "@/styles/Categories.module.css";
 
-export default function Category({ number, title, planet, setText }) {
-  //overview, structure, geology
+export default function Category({ number, title, planet, setText, setSourceURL }) {
+
   let handleClick = (e) => {
     if (title === 'overview') {
       setText(planet.overview.content);
-      //also update source?
+      setSourceURL(planet.overview.source);
     } else if (title === 'internal structure') {
       setText(planet.structure.content);
+      setSourceURL(planet.structure.source);
     } else {
-      setText(planet.geology.content)
+      setText(planet.geology.content);
+      setSourceURL(planet.geology.source);
     }
 
   };
